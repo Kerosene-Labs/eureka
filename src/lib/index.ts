0aea8524824422c466564dba7f912893f87682dd
hlafaille <hlafaille@proton.me>
2025-01-05 13:12:59 -0500

    adding functions

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,30 @@
-// Reexport your entry components here
+// buttons
+export {default as EButton} from '$lib/button/EButton.svelte';
+
+// containers
+export {default as ECard} from '$lib/container/ECard.svelte';
+export {default as EModal} from '$lib/container/EModal.svelte';
+
+// inputs
+export {default as EDateInput} from '$lib/input/EDateInput.svelte';
+export {default as ENumberInput} from '$lib/input/ENumberInput.svelte';
+export {default as ETextInput} from '$lib/input/ETextInput.svelte';
+
+// layout
+export {default as EHorizontalSeparator} from '$lib/layout/EHorizontalSeparator.svelte';
+
+// table
+export {default as ETable} from '$lib/table/ETable.svelte';
+export {default as ETableRow} from '$lib/table/ETableRow.svelte';
+
+// toast
+export {default as EToast} from '$lib/toast/EToast.svelte';
+export {default as EToastQueue} from '$lib/toast/EToastQueue.svelte';
+export { addToToastQueue, ToastType, toastQueue } from '$lib/toast/toastControl.js';
+export type { ToastRequest } from '$lib/toast/toastControl.js';
+
+// spinner
+export {default as ESpinner} from '$lib/ESpinner.svelte';
+
+// helpers
+export { getOrdinal, formatCurrency, isEmpty } from '$lib/valueHelpers.js'
